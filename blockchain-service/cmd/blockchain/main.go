26056c6eaad9d6b538a83126c54eb8770c78033c
Jun Ming Khong <junming.khong@grabtaxi.com>
2023-07-16 16:57:23 +0800

    feat: add chaincode package

--- blockchain-service/cmd/blockchain/main.go
+++ blockchain-service/cmd/blockchain/main.go
@@ -7,12 +7,21 @@ package main
 
 import (
 	"flag"
+	"github.com/hyperledger/fabric-contract-api-go/contractapi"
 	"github.com/julkhong/blockchain-server/blockchain-service/handlers"
+	"github.com/julkhong/blockchain-server/blockchain-service/logic"
 	"github.com/julkhong/blockchain-server/blockchain-service/svc/server"
-
+	log "github.com/sirupsen/logrus"
 )
 
 func main() {
+	amountTransferChaincode, err := contractapi.NewChaincode(&logic.AmountTransferContract{})
+	if err != nil {
+		log.Panicf("Error creating amount transfer chaincode: %v", err)
+	}
+	if startErr := amountTransferChaincode.Start(); startErr != nil{
+		log.Panicf("Error starting amount transfer chaincode: %v", startErr)
+	}
 	// Update addresses if they have been overwritten by flags
 	flag.Parse()
 
