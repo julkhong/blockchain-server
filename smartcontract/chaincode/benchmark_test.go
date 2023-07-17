35b64f9e373d7270a19587c261bc3686a3bd34ef
Jun Ming Khong <junming.khong@grabtaxi.com>
2023-07-17 11:21:47 +0800

    feat: for demo only

--- smartcontract/chaincode/benchmark_test.go
+++ smartcontract/chaincode/benchmark_test.go
@@ -1,59 +1,53 @@
 package chaincode
 
-import (
-	"testing"
-
-	"github.com/stretchr/testify/require"
-)
-
-const testPublicKey = "-----BEGIN PUBLIC KEY-----\nMIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAt/LtqNLiYkmf4jjO3XXG\nD6fI5gg30yMLc+uD1MKuKzrCIBg7K2K9DWWwozqI4hfU8bV8wJt8fXvF7wF4U+eO\nsUzYyxNqAlYZW4r9DQ77RyQtskU+x+MT7E8aPjVswm1Yv9Fe7UsjDqTm2ugjDX3Z\nRds7Qbgs5TwHkBNlrrDGDLm/BH2N3BpC9Ry0ToI8Z9T7nhiAA3Fz1HqQuIi6A1IK\n1G0S5KT4a/yFlKklkBYol/y+DZx2fWx13dWkKofu30AVAGUuewU9HEt9zLr/U55w\n9WwRr9htB79j0TmqCkncSnwqHQxvR/7zL7COV3v2G5rJ2anb7wYh0OHNJ1jMaXz7\nEwIDAQAB\n-----END PUBLIC KEY-----\n"
-
-func BenchmarkSend(b *testing.B) {
-	// Create a new instance of the smart contract
-	contract := &AmountTransferContract{}
-	ctx := &MockTransactionContext{
-		Stub:          &MockStub{StateMap: make(map[string][]byte)},
-		TransactionID: "test-transaction",
-		ChannelID:     "test-channel",
-		Contract:      contract,
-	}
-
-	// Initialize the contract
-	accountA, err := contract.InitM(ctx, testPublicKey, 10000000000000)
-	require.NoError(b, err)
-	accountB, err := contract.InitM(ctx, testPublicKey, 10000000000000)
-	require.NoError(b, err)
-
-	// Perform the benchmarking
-	b.ResetTimer()
-	for i := 0; i < b.N; i++ {
-		err = contract.SendM(ctx, accountA, accountB, 1)
-		require.NoError(b, err)
-	}
-}
-
-func BenchmarkGetBalance(b *testing.B) {
-	// Create a new instance of the smart contract
-	contract := &AmountTransferContract{}
-	ctx := &MockTransactionContext{
-		Stub:          &MockStub{StateMap: make(map[string][]byte)},
-		TransactionID: "test-transaction",
-		ChannelID:     "test-channel",
-		Contract:      contract,
-	}
-	// Initialize the contract
-	accountA, err := contract.InitM(ctx, testPublicKey, 100)
-	require.NoError(b, err)
-	expectedRes := &Account{
-		ID:        accountA,
-		Balance:   100,
-		PublicKey: testPublicKey,
-	}
-	// Perform the benchmarking
-	b.ResetTimer()
-	for i := 0; i < b.N; i++ {
-		res, getBalanceErr := contract.GetBalanceM(ctx, accountA)
-		require.NoError(b, getBalanceErr)
-		require.Equal(b, res, expectedRes.Balance)
-	}
-}
+//const testPublicKey = "-----BEGIN PUBLIC KEY-----\nMIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAt/LtqNLiYkmf4jjO3XXG\nD6fI5gg30yMLc+uD1MKuKzrCIBg7K2K9DWWwozqI4hfU8bV8wJt8fXvF7wF4U+eO\nsUzYyxNqAlYZW4r9DQ77RyQtskU+x+MT7E8aPjVswm1Yv9Fe7UsjDqTm2ugjDX3Z\nRds7Qbgs5TwHkBNlrrDGDLm/BH2N3BpC9Ry0ToI8Z9T7nhiAA3Fz1HqQuIi6A1IK\n1G0S5KT4a/yFlKklkBYol/y+DZx2fWx13dWkKofu30AVAGUuewU9HEt9zLr/U55w\n9WwRr9htB79j0TmqCkncSnwqHQxvR/7zL7COV3v2G5rJ2anb7wYh0OHNJ1jMaXz7\nEwIDAQAB\n-----END PUBLIC KEY-----\n"
+//
+//func BenchmarkSend(b *testing.B) {
+//	// Create a new instance of the smart contract
+//	contract := &AmountTransferContract{}
+//	ctx := &MockTransactionContext{
+//		Stub:          &MockStub{StateMap: make(map[string][]byte)},
+//		TransactionID: "test-transaction",
+//		ChannelID:     "test-channel",
+//		Contract:      contract,
+//	}
+//
+//	// Initialize the contract
+//	accountA, err := contract.InitM(ctx, testPublicKey, 10000000000000)
+//	require.NoError(b, err)
+//	accountB, err := contract.InitM(ctx, testPublicKey, 10000000000000)
+//	require.NoError(b, err)
+//
+//	// Perform the benchmarking
+//	b.ResetTimer()
+//	for i := 0; i < b.N; i++ {
+//		err = contract.SendM(ctx, accountA, accountB, 1)
+//		require.NoError(b, err)
+//	}
+//}
+//
+//func BenchmarkGetBalance(b *testing.B) {
+//	// Create a new instance of the smart contract
+//	contract := &AmountTransferContract{}
+//	ctx := &MockTransactionContext{
+//		Stub:          &MockStub{StateMap: make(map[string][]byte)},
+//		TransactionID: "test-transaction",
+//		ChannelID:     "test-channel",
+//		Contract:      contract,
+//	}
+//	// Initialize the contract
+//	accountA, err := contract.InitM(ctx, testPublicKey, 100)
+//	require.NoError(b, err)
+//	expectedRes := &Account{
+//		ID:        accountA,
+//		Balance:   100,
+//		PublicKey: testPublicKey,
+//	}
+//	// Perform the benchmarking
+//	b.ResetTimer()
+//	for i := 0; i < b.N; i++ {
+//		res, getBalanceErr := contract.GetBalanceM(ctx, accountA)
+//		require.NoError(b, getBalanceErr)
+//		require.Equal(b, res, expectedRes.Balance)
+//	}
+//}
--- smartcontract/chaincode/mock_contract.go
+++ smartcontract/chaincode/mock_contract.go
@@ -1,131 +1,123 @@
 package chaincode
 
-import (
-	"encoding/json"
-	"fmt"
-
-	"github.com/google/uuid"
-	"github.com/hyperledger/fabric-contract-api-go/contractapi"
-)
-
-type AmountTransferContractM struct {
-	contractapi.Contract
-}
-
-// InitM initializes an account
-func (a *AmountTransferContract) InitM(ctx TransactionContextInterface, publicKey string, amount uint64) (string, error) {
-	uniqueID := uuid.New()
-	accountID := uniqueID.String()
-	newAccount := Account{
-		ID:        accountID,
-		Balance:   amount,
-		PublicKey: publicKey,
-	}
-	accountJSON, err := json.Marshal(newAccount)
-	if err != nil {
-		return "", fmt.Errorf("failed to serialize new account: %w", err)
-	}
-	err = ctx.GetStub().PutState(accountID, accountJSON)
-	if err != nil {
-		return "", fmt.Errorf("failed to put new account state: %w", err)
-	}
-	return accountID, nil
-}
-
-func (a *AmountTransferContract) InitAccountsM(ctx TransactionContextInterface) error {
-	accounts := []Account{
-		{ID: "acc1", Balance: 100, PublicKey: "key1"},
-		{ID: "acc2", Balance: 100, PublicKey: "key2"},
-		{ID: "acc3", Balance: 100, PublicKey: "key3"},
-		{ID: "acc4", Balance: 100, PublicKey: "key4"},
-		{ID: "acc5", Balance: 100, PublicKey: "key5"},
-		{ID: "acc6", Balance: 100, PublicKey: "key6"},
-	}
-
-	for _, asset := range accounts {
-		assetJSON, err := json.Marshal(asset)
-		if err != nil {
-			return fmt.Errorf("failed to serialize new account: %w", err)
-		}
-
-		err = ctx.GetStub().PutState(asset.ID, assetJSON)
-		if err != nil {
-			return fmt.Errorf("failed to put new account state: %w", err)
-		}
-	}
-
-	return nil
-}
-
-// SendM transfers amount from sender to a receiver
-func (a *AmountTransferContract) SendM(ctx TransactionContextInterface, senderID, receiverID string, amount uint64) error {
-	senderJSON, err := ctx.GetStub().GetState(senderID)
-	if err != nil {
-		return fmt.Errorf("failed to read sender account from the world state: %w", err)
-	}
-	if senderJSON == nil {
-		return fmt.Errorf("account with ID %s does not exist", senderID)
-	}
-
-	receiverJSON, err := ctx.GetStub().GetState(receiverID)
-	if err != nil {
-		return fmt.Errorf("failed to read receiver account from the world state: %w", err)
-	}
-	if receiverJSON == nil {
-		return fmt.Errorf("account with ID %s does not exist", receiverID)
-	}
-
-	account := new(Account)
-	err = json.Unmarshal(senderJSON, account)
-	if err != nil {
-		return fmt.Errorf("failed to deserialize sender account JSON: %w", err)
-	}
-	balance := int(account.Balance) - int(amount)
-	if balance < 0 {
-		return fmt.Errorf("failed to transfer as sender has insufficient balance")
-	}
-	account.Balance = uint64(balance)
-	senderJSON, err = json.Marshal(account)
-	if err != nil {
-		return fmt.Errorf("failed to serialize sender account: %w", err)
-	}
-	err = ctx.GetStub().PutState(senderID, senderJSON)
-	if err != nil {
-		return fmt.Errorf("failed to update sender account in the world state: %w", err)
-	}
-
-	account = new(Account)
-	err = json.Unmarshal(receiverJSON, account)
-	if err != nil {
-		return fmt.Errorf("failed to deserialize receiver account JSON: %w", err)
-	}
-	balance = int(account.Balance) + int(amount)
-	account.Balance = uint64(balance)
-	receiverJSON, err = json.Marshal(account)
-	if err != nil {
-		return fmt.Errorf("failed to serialize receiver account: %w", err)
-	}
-	err = ctx.GetStub().PutState(receiverID, receiverJSON)
-	if err != nil {
-		return fmt.Errorf("failed to update receiver account in the world state: %w", err)
-	}
-	return nil
-}
-
-func (a *AmountTransferContract) GetBalanceM(ctx TransactionContextInterface, accountID string) (uint64, error) {
-	accountJSON, err := ctx.GetStub().GetState(accountID)
-	if err != nil {
-		return 0, fmt.Errorf("failed to read account from the world state: %w", err)
-	}
-	if accountJSON == nil {
-		return 0, fmt.Errorf("account with ID %s does not exist", accountID)
-	}
-
-	account := new(Account)
-	err = json.Unmarshal(accountJSON, account)
-	if err != nil {
-		return 0, fmt.Errorf("failed to deserialize account JSON: %w", err)
-	}
-
-	return account.Balance, nil
-}
+//type AmountTransferContractM struct {
+//	contractapi.Contract
+//}
+//
+//// InitM initializes an account
+//func (a *AmountTransferContract) InitM(ctx TransactionContextInterface, publicKey string, amount uint64) (string, error) {
+//	uniqueID := uuid.New()
+//	accountID := uniqueID.String()
+//	newAccount := Account{
+//		ID:        accountID,
+//		Balance:   amount,
+//		PublicKey: publicKey,
+//	}
+//	accountJSON, err := json.Marshal(newAccount)
+//	if err != nil {
+//		return "", fmt.Errorf("failed to serialize new account: %w", err)
+//	}
+//	err = ctx.GetStub().PutState(accountID, accountJSON)
+//	if err != nil {
+//		return "", fmt.Errorf("failed to put new account state: %w", err)
+//	}
+//	return accountID, nil
+//}
+//
+//func (a *AmountTransferContract) InitAccountsM(ctx TransactionContextInterface) error {
+//	accounts := []Account{
+//		{ID: "acc1", Balance: 100, PublicKey: "key1"},
+//		{ID: "acc2", Balance: 100, PublicKey: "key2"},
+//		{ID: "acc3", Balance: 100, PublicKey: "key3"},
+//		{ID: "acc4", Balance: 100, PublicKey: "key4"},
+//		{ID: "acc5", Balance: 100, PublicKey: "key5"},
+//		{ID: "acc6", Balance: 100, PublicKey: "key6"},
+//	}
+//
+//	for _, asset := range accounts {
+//		assetJSON, err := json.Marshal(asset)
+//		if err != nil {
+//			return fmt.Errorf("failed to serialize new account: %w", err)
+//		}
+//
+//		err = ctx.GetStub().PutState(asset.ID, assetJSON)
+//		if err != nil {
+//			return fmt.Errorf("failed to put new account state: %w", err)
+//		}
+//	}
+//
+//	return nil
+//}
+//
+//// SendM transfers amount from sender to a receiver
+//func (a *AmountTransferContract) SendM(ctx TransactionContextInterface, senderID, receiverID string, amount uint64) error {
+//	senderJSON, err := ctx.GetStub().GetState(senderID)
+//	if err != nil {
+//		return fmt.Errorf("failed to read sender account from the world state: %w", err)
+//	}
+//	if senderJSON == nil {
+//		return fmt.Errorf("account with ID %s does not exist", senderID)
+//	}
+//
+//	receiverJSON, err := ctx.GetStub().GetState(receiverID)
+//	if err != nil {
+//		return fmt.Errorf("failed to read receiver account from the world state: %w", err)
+//	}
+//	if receiverJSON == nil {
+//		return fmt.Errorf("account with ID %s does not exist", receiverID)
+//	}
+//
+//	account := new(Account)
+//	err = json.Unmarshal(senderJSON, account)
+//	if err != nil {
+//		return fmt.Errorf("failed to deserialize sender account JSON: %w", err)
+//	}
+//	balance := int(account.Balance) - int(amount)
+//	if balance < 0 {
+//		return fmt.Errorf("failed to transfer as sender has insufficient balance")
+//	}
+//	account.Balance = uint64(balance)
+//	senderJSON, err = json.Marshal(account)
+//	if err != nil {
+//		return fmt.Errorf("failed to serialize sender account: %w", err)
+//	}
+//	err = ctx.GetStub().PutState(senderID, senderJSON)
+//	if err != nil {
+//		return fmt.Errorf("failed to update sender account in the world state: %w", err)
+//	}
+//
+//	account = new(Account)
+//	err = json.Unmarshal(receiverJSON, account)
+//	if err != nil {
+//		return fmt.Errorf("failed to deserialize receiver account JSON: %w", err)
+//	}
+//	balance = int(account.Balance) + int(amount)
+//	account.Balance = uint64(balance)
+//	receiverJSON, err = json.Marshal(account)
+//	if err != nil {
+//		return fmt.Errorf("failed to serialize receiver account: %w", err)
+//	}
+//	err = ctx.GetStub().PutState(receiverID, receiverJSON)
+//	if err != nil {
+//		return fmt.Errorf("failed to update receiver account in the world state: %w", err)
+//	}
+//	return nil
+//}
+//
+//func (a *AmountTransferContract) GetBalanceM(ctx TransactionContextInterface, accountID string) (uint64, error) {
+//	accountJSON, err := ctx.GetStub().GetState(accountID)
+//	if err != nil {
+//		return 0, fmt.Errorf("failed to read account from the world state: %w", err)
+//	}
+//	if accountJSON == nil {
+//		return 0, fmt.Errorf("account with ID %s does not exist", accountID)
+//	}
+//
+//	account := new(Account)
+//	err = json.Unmarshal(accountJSON, account)
+//	if err != nil {
+//		return 0, fmt.Errorf("failed to deserialize account JSON: %w", err)
+//	}
+//
+//	return account.Balance, nil
+//}
